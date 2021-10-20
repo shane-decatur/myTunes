9381e59650eb071a4a51720b4d14378acd5d07a4
Adnan <54964283+adnanq2004@users.noreply.github.com>
2021-10-19 21:36:33 -0400

    Update songnode.h

--- songnode.h
+++ songnode.h
@@ -8,4 +8,10 @@ struct song_node {
 
 struct song_node * insert_order(char * name, char * artist);
 
+void print_list(struct song_node * start);
 
+struct linkednode * insert_front(struct song_node * exm, char * s, char * st);
+
+struct linkednode * free_list(struct song_node * exm);
+
+struct linkednode * remove_node(struct song_node *front, char * s);
