30bcdcf7b76b12dedd60b9cd41de0ec1b4f0d5c9
Adnan <54964283+adnanq2004@users.noreply.github.com>
2021-10-24 15:02:53 -0400

    Update main.c

--- main.c
+++ main.c
@@ -59,6 +59,40 @@ int main(){
   printf("All of the library: \n");
   print_library(library);
   printf("-------------------------------------------\n");
+  
+  struct song_node * list = make("The World Stands Still", "Biley Renner");
+  printf("\n");
+  print_list(list);
+  struct song_node * toadd;
+  toadd = make("The End Comes Soon", "Biley Renner");
+  list = insert_order(list, toadd);
+  printf("\n");
+  print_list(list);
+  toadd = make("The Trilogy Ruptures", "Biley Benner");
+  list = insert_order(list, toadd);
+  printf("\n");
+  print_list(list);
+  toadd = make("The Trilogy Ruptures", "Biley Renner");
+  list = insert_order(list, toadd);
+  printf("\n");
+  print_list(list);
+  list = remove_node(list, "Biley Benner", "The Trilogy Ruptures");
+  printf("\n");
+  print_list(list);
+  toadd = make("Hallelujah", "Daddoms Science");
+  list = insert_front(list, toadd);
+  printf("\n");
+  print_list(list);
+
+  // struct song_node * rando = find_randomsong(list);
+  struct song_node * rando = find_song(list, "The End Comes Soon", "Biley Renner");
+  printf("\n");
+  print_struct(rando);
+  printf("\n");
+  rando = find_randomsong(list);
+  printf("\n");
+  print_struct(rando);
+  printf("\n");
 
   return 0;
 }
